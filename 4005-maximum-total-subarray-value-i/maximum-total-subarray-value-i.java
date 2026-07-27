98789a8ac721517798097c81e610ba42289df4fc
Jannat kataria <jannatkataria02@gmail.com>
2026-07-27 22:06:23 +0530

    Time: 1 ms (100.00%) | Memory: 62.4 MB (33.67%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num > max) max = num;
+            if (num < min) min = num;
+        }
+        long diff=max-min;
+        return diff*k;
+    }
+}
