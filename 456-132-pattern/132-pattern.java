e1cc18f3823d8aaf3db0d4489c0c0f700a513554
Jannat kataria <jannatkataria02@gmail.com>
2026-08-10 20:12:18 +0530

    Time: 39 ms (38.27%) | Memory: 98.6 MB (54.40%) - LeetSync

---
+++ 456-132-pattern/132-pattern.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < secondMax) {
+                return true;
+            }
+            
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                secondMax = stack.pop();
+            }
+            
+            stack.push(nums[i]);
+        }
+        return false;
+    }
+}
